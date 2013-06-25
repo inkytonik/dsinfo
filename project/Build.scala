2a35f9e1e4980002b389744474fefee66ca7c443
Tony Sloane <inkytonik@gmail.com>
2013-06-26 09:17:34 +1000

    Fix typo in copyright notice

--- project/Build.scala
+++ project/Build.scala
@@ -4,12 +4,12 @@
  * Copyright (C) 2013 Anthony M Sloane, Macquarie University.
  * Copyright (C) 2013 Matthew Roberts, Macquarie University.
  *
- * dsnaem is free software: you can redistribute it and/or modify it under
+ * dsname is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * dsnaem is distributed in the hope that it will be useful, but WITHOUT ANY
+ * dsname is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
  * more details.
